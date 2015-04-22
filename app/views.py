0c5d5603d60b1f5fc9630967b5ad88ed7c3507ba
Manuel González <magandrez@gmail.com>
2015-04-22 22:48:28 +0300

    Templates
    Playing a little with templates in Flask.

--- app/views.py
+++ app/views.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Manuel'}
+    posts = [  # fake array of posts
+        {
+            'author': {'nickname': 'Ana'},
+            'body': 'It was nice to see you!'
+        },
+        {
+            'author': {'nickname': 'Francisco'},
+            'body': 'I am hungry!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
