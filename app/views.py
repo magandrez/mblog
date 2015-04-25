36dfe50c4780172dbab89b4f63181e50ec5df730
Manuel González <magandrez@gmail.com>
2015-04-25 19:02:36 +0300

    Web forms
    Taking a brief look at web forms.
    Modified gitignore to cover __pycache__/ in root.

--- app/views.py
+++ app/views.py
@@ -1,19 +1,16 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'nickname': 'Manuel'}
-    posts = [  # fake array of posts
-        {
-            'author': {'nickname': 'Ana'},
-            'body': 'It was nice to see you!'
-        },
-        {
-            'author': {'nickname': 'Francisco'},
-            'body': 'I am hungry!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
